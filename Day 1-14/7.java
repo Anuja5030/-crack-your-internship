a3041df0b1b87a5b05512c569292a07e37cc445c
Anuja babhulkar <anujababhulkar23@gmail.com>
2024-07-14 21:58:21 +0530

    14 july

--- Day 1-14/7.java
+++ Day 1-14/7.java
@@ -12,3 +12,18 @@ class Solution {
     return index;
     }
 }
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy_stock=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<buy_stock){
+                buy_stock=prices[i];
+            }else if(prices[i]-buy_stock>profit){
+                profit=prices[i]-buy_stock;
+            }
+        }
+        return profit;
+        
+    }
+}
